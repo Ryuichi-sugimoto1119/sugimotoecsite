21c72d3eb1815c5436f9d33f9d4c66653a3c110d
EC2 Default User <ec2-user@ip-172-31-0-29.ap-northeast-1.compute.internal>
2021-02-22 07:11:53 +0000

    前半終了

---
+++ app/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Overtrue\LaravelFavorite\Traits\Favoriteable;
+use Kyslik\ColumnSortable\Sortable;
+
+class Product extends Model
+{
+    
+    use Favoriteable, Sortable;
+    
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
+    
+    public function reviews()
+    {
+        return $this->hasMany('App\Review');
+    }
+}
